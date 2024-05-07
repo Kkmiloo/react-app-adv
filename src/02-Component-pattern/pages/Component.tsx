19b9c4ee94e0ecc7c796e8c3ff02e59fead33dbe
camilo <camilo.reyes_granada@uao.edu.co>
2024-05-06 21:49:07 -0500

    Component parent

---
+++ src/02-Component-pattern/pages/Component.tsx
@@ -0,0 +1,11 @@
+export const About = () => {
+  return <div>About</div>;
+};
+
+export const Home = () => {
+  return <div>home</div>;
+};
+
+export const User = () => {
+  return <div>user</div>;
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,6 @@
-import { LazyExoticComponent, lazy } from 'react';
+import { LazyExoticComponent } from 'react';
+
+import { About, Home, User } from '../02-Component-pattern/pages/Component';
 //import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 type JSXComponent = () => JSX.Element;
@@ -10,28 +12,23 @@ interface Route {
   Component: LazyExoticComponent<JSXComponent> | JSXComponent;
 }
 
-const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
-
 export const routes: Route[] = [
   {
-    path: 'lazy1',
-    to: '/lazy1',
-    Component: Lazy1,
-    name: 'Lazy 1',
+    path: '/home',
+    to: '/home',
+    Component: Home,
+    name: 'Home',
   },
   {
-    path: 'lazy2',
-    to: '/lazy2',
-    Component: Lazy2,
-    name: 'Lazy 2',
+    to: '/users',
+    path: 'users',
+    Component: User,
+    name: 'Users',
   },
-
   {
-    path: 'lazy3',
-    to: '/lazy3',
-    Component: Lazy3,
-    name: 'Lazy 3',
+    to: '/about',
+    path: 'about',
+    Component: About,
+    name: 'About',
   },
 ];
